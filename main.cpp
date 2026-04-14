1e5083a3b34d7024ebfeec6e79c959895936cb42
Rinz0n <rinzon16@gmail.com>
2026-04-14 22:54:35 +0400

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,91 +1,42 @@
 #include "Header.h"
 #include "Header.cpp"
 
-int main() {
+int main(){
     using namespace std;
     setlocale(LC_ALL, "RU");
-    cout << "Выберите задачу (1-5):" << endl;
-    int choice;
-    cin >> choice;
-    switch (choice) {
-    case 1: {
-        Stack stack1, stack2;
-        cout << endl << "Создание первого стека:" << endl;
-        fillStructure(stack1, "первый стек");
-        cout << endl << "Создание второго стека:" << endl;
-        fillStructure(stack2, "второй стек");
-        cout << endl << "Первый стек:" << endl;
-        stack1.show();
-        cout << "Второй стек:" << endl;
-        stack2.show();
-        stack2.merge(stack1);
-        cout << endl << "Результат (второй стек после слияния):" << endl;
-        stack2.show();
+    int number, cnt;
+    cout << "Введите номер задачи:" << endl;
+    cin >> number;
+    switch (number) {
+    case 1:{
+        addinfile(cnt);
+        distrinfile(cnt);
+        print();
         break;
     }
-
-    case 2: {
-        Queue queue;
-        fillStructure(queue, "очередь");
-        cout << endl << "Исходная очередь:" << endl;
-        queue.show();
-        int N;
-        cout << endl << "Сколько элементов извлечь? ";
-        cin >> N;
-        queue.extractElements(N);
-        cout << endl << "Очередь после извлечения:" << endl;
-        queue.show();
+    case 2:{
+        addinfile(cnt);
+        deletenegativenumbers(cnt);
         break;
     }
-
-    case 3: {
-        List list;
-        fillStructure(list, "список");
-        cout << endl << "Исходный список:" << endl;   
-        list.show();
-        Node* ptr = list.getPtrToElement(7);
-        if (ptr != nullptr) {
-            cout << "Указатель на 7-й элемент (Head → ... → 7-й): " << ptr << endl;
-            cout << "Значение 7-го элемента: " << ptr->data << endl;
-        }
-        else {
-            cout << "В списке меньше 7 элементов!" << endl;
-        }
+    case 3:{
+        int n;
+        string s;
+        cout << "Введите строку S:" << endl;
+        cin >> s;
+        cout << "Введите целое число N(>0):" << endl;
+        cin >> n;
+        createfile(n);
         break;
     }
-
-    case 4: {
-        List list;
-        fillStructure(list, "список");
-        cout << endl << "Исходный список:" << endl;
-        list.show();
-        int M;
-        cout << "Введите значение M для вставки: ";
-        cin >> M;
-        list.insertAfterEveryThird(M);
-        cout << endl << "Список после вставки:" << endl;
-        list.show();
+    case 4:{
+        string s;
+        int i = 0;
+        cout << "Введите выражение:" << endl;
+        cin >> s;
+        cout << "Значение выражения: " << expr(s,i);
         break;
     }
-
-    case 5: {
-    List original;
-    fillStructure(original, "исходный список");
-    cout << endl << "Исходный список:" << endl;
-    original.show();
-    int M;
-    cout << "Введите значение M для вставки в упорядоченный список: ";
-    cin >> M;
-    List* copy = original.copySorted(M);
-    cout << endl << "Копия списка с вставленным значением " << M << ":" << endl;
-    copy->show();
-    cout << "Исходный список (не изменился):" << endl;
-    original.show();
-    delete copy; 
-    break;
-}
-    default:
-        cout << "Неверный номер задачи!" << endl;
     }
     return 0;
 }
