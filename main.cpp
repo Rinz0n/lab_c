6fb5cc88107f4768c266f35e4327d76b0a3f66a3
Rinz0n <rinzon16@gmail.com>
2026-04-14 23:07:27 +0400

    lab12

--- main.cpp
+++ main.cpp
@@ -1,62 +1,104 @@
 #include "Header.h"
 #include "Header.cpp"
+#include <limits>
 
 int main() {
     using namespace std;
     setlocale(LC_ALL, "RU");
-    cout << "Выберите задачу (1-5):" << endl;
+    cout << endl << "Выберите задачу:" << endl;
     int choice;
-    cin >> choice;
-    
+    while (!(cin >> choice) || choice < 1 || choice > 4) {
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        cout << "Ошибка! Введите число от 1 до 4: ";
+    }
     switch (choice) {
     case 1: {
-        RingList list;
-        fillStructure(list, "кольцо");
-        cout << "\nИсходное кольцо:" << endl;
-        list.show();
-        list.task1("output1.txt");
+        vector<int> V;
+        deque<int> D;
+        list<int> L;
+        cout << endl << "Заполнение вектора V " << endl;
+        fillContainer(V, "вектор");
+        cout << endl << "Заполнение дека D " << endl;
+        fillContainer(D, "дек");
+        cout << endl <<"Заполнение списка L " << endl;
+        fillContainer(L, "список");
+        cout << endl << "Исходные контейнеры:" << endl;
+        cout << "V: "; printContainer(V);
+        cout << "D: "; printContainer(D);
+        cout << "L: "; printContainer(L);
+        task7(V, D, L);
+        cout << endl << "После замены средних элементов:" << endl;
+        cout << "V: "; printContainer(V);
+        cout << "D: "; printContainer(D);
+        cout << "L: "; printContainer(L);
         break;
     }
     
     case 2: {
-        DoubleList1 list;
-        fillStructure(list, "список");
-        cout << "\nИсходный список:" << endl;
-        list.show();
-        list.task2();
+        vector<int> V;
+        deque<int> D;
+        cout << endl << "Заполнение вектора V " << endl;
+        fillContainer(V, "вектор");
+        cout << endl << "Заполнение дека D " << endl;
+        fillContainer(D, "дек");
+        cout << endl << "Исходные контейнеры:" << endl;
+        cout << "V: "; printContainer(V);
+        cout << "D: "; printContainer(D);
+        task13(V, D);
+        cout << endl << "После преобразования:" << endl;
+        cout << "V: "; printContainer(V);
+        cout << "D: "; printContainer(D);
         break;
     }
     
     case 3: {
-        DoubleList2 list;
-        fillStructure(list, "список");
-        cout << "\nИсходный список:" << endl;
-        list.show();
-        list.task3("output3.txt");
+        vector<int> V;
+        cout << endl << "Заполнение вектора V " << endl;
+        fillContainer(V, "вектор");
+        cout << endl << "Исходный вектор:" << endl;
+        cout << "V: "; printContainer(V);
+        task21(V);
+        cout << endl << "После удаления трех средних элементов:" << endl;
+        cout << "V: "; printContainer(V);
         break;
     }
     
     case 4: {
-        ListWithBarrier list;
-        fillStructure(list, "список");
-        cout << "\nИсходный список:" << endl;
-        list.show();
-        int count;
-        Node* newCurrent;
-        list.task4(count, newCurrent);
-        cout << "Количество элементов: " << count << endl;
-        cout << "Новый адрес текущего элемента (барьерный): " << newCurrent << endl;
-        cout << "Список после обнуления нечетных:" << endl;
-        list.show();
-        break;
-    }
-    
-    case 5: {
-        DoubleList3 list;
-        fillStructure(list, "список");
-        cout << "\nИсходный список:" << endl;
-        list.show();
-        list.task5();
+        string name;
+        string symbols;
+        cout << endl << "Введите имя файла: ";
+        cin >> name;
+        cout << "Выберите способ ввода символов:" << endl;
+        cout << "1 - С клавиатуры" << endl;
+        cout << "2 - Из текстового файла" << endl;
+        cout << "Ваш выбор: ";
+        int inputChoice;
+        while (!(cin >> inputChoice) || (inputChoice != 1 && inputChoice != 2)) {
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+            cout << "Ошибка! Введите 1 или 2: ";
+        }
+        if (inputChoice == 1) {
+            cout << "Введите набор символов: ";
+            cin >> symbols;
+        } else {
+            string inputFile;
+            cout << "Введите имя файла с символами: ";
+            cin >> inputFile;
+            ifstream file(inputFile);
+            if (file.is_open()) {
+                getline(file, symbols);
+                file.close();
+                cout << "Символы загружены из файла: " << symbols << endl;
+            } else {
+                cout << "Ошибка открытия файла! Используем ввод с клавиатуры." << endl;
+                cout << "Введите набор символов: ";
+                cin >> symbols;
+            }
+        }
+        task17(name, symbols);
+        cout << "Результат записан в файл " << name << endl;
         break;
     }
     
